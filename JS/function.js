431100e1f3fea20b65742e48d01d07e0b0123e0b
niyonshuti rushema jean claude <jniyonshutirushema@gmail.com>
2026-07-21 11:20:18 +0200

    add changed  JS/function.js

--- JS/function.js
+++ JS/function.js
@@ -18,6 +18,43 @@ console.log(addElements());
 
 
 function greetUser() {
-    return "Hello, claude!"
+  const personalName = 'claude'
+  const sentence = `hello ${personalName}!`
+  return sentence 
 }
 console.log(greetUser())
+
+
+function nameOfAge(passedYear, nawTame) {
+    return nawTame - passedYear;
+}
+
+console.log(nameOfAge(2000, 2026));
+
+
+function studentss(name, age, height) {
+    const information = {
+        Name: name,
+        Age: age,
+        Height: height
+    };
+
+    return information;
+}
+
+console.log(studentss("rushema", 30, 1.70));
+console.log(studentss("kamana",28,1.69));
+console.log(studentss("mupenzi",40,1.82));
+console.log(studentss("havuga",45,1.90));
+console.log(studentss("kabera",34,1.78));
+
+
+
+function numberd(a, b) {
+       
+      const  sum = a + b
+
+    return sum;
+}
+
+console.log(numberd(7, 5));
