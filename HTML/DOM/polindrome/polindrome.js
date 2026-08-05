144dfcefba48b5a589b648dbb2dd9c87ef6b9dcf
niyonshuti rushema jean claude <jniyonshutirushema@gmail.com>
2026-08-05 14:22:14 +0200

    add changed  HTML/DOM/polindrome/polindrome.html,HTML/DOM/polindrome/polindrome.js

--- HTML/DOM/polindrome/polindrome.js
+++ HTML/DOM/polindrome/polindrome.js
@@ -2,27 +2,31 @@
 const input = document.querySelector("#inputs");
 const button = document.querySelector("#check");
 const answer = document.querySelector("#result");
+const compare = document.querySelector("#compare")
 
 
-function chechPolindrome(){
-    const trimededText = input.value.trim();
+function chechPolindrome(text) {
+    const trimededText = text.trim();
     const cleannedText = trimededText.replace(/[^a-zA-Z0-9]/g, '');
-    result.innerHTML = cleannedText;
+    const smallText = cleannedText.toLowerCase();
+    const wordArray = smallText.split('');
+  
 
 
     const reversedWordArray = wordArray.reverse();
     const joinReversedWordArray = reversedWordArray.join('');
 
-    compare . innerHTML = `${smallText} and ${joinReversedWordArray}`;
+    compare.innerHTML = `${smallText} and ${joinReversedWordArray}`;
 
-    result.style.color = "#4EA281";
-    if(smallText === joinReversedWordArray) return result.innerHTML = "this text ispolindrome"
-    result.style.color = "#F16464";
-     return result.innerHTML = "this text is not polindrome";
-
-     
+    answer.style.color = "#4EA281";
+    if (smallText === joinReversedWordArray)
+        answer.innerHTML = "this text ispolindrome"
+    answer.style.color = "#F16464";
+    return answer.innerHTML = "this text is not polindrome";
 }
-input.addEventListener("input" ,function(){
+
+
+input.addEventListener("input", function () {
     answer.innerHTML = "no checked yet!!";
     answer.style.color = "grey"
 });
@@ -32,16 +36,19 @@ function checksText() {
 
     if (text === "") {
         answer.innerHTML = "Hey!! Insert text to check.";
-        answer.style.color ="red";
+        answer.style.color = "red";
 
     } else if (text.length < 3) {
         answer.innerHTML = "Hey!! Text should contain at least 3 characters.";
-        answer.style .color ="red"
-       
+        answer.style.color = "red"
+
     } else {
         answer.innerHTML = text;
         answer.style.color = "grey"
     }
+
+    chechPolindrome(text)
 }
 
 button.addEventListener("click", checksText);
+
